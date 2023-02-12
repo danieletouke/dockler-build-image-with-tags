f1470826d592cea16e8bc5f14db7784621f00581
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-02-11 20:56:31 -0500

    upddate push2

--- push2.sh
+++ push2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+repo_name=danieletouk3
 image_name=app
 num=`docker images | sort -r | awk '{print $2}' | head -1`
 version=$(echo $num + 1 | bc -l)
 
-docker build -t $image_name:$version .
+docker build -t $repo_name/$image_name:$version .
 
 echo "old version docker images"
 docker images
