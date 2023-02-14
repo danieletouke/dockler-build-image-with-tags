027ec0315f92eb179258d46e409f8b4296615c59
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-02-14 16:32:42 -0500

    fixing bug in build_docker_image

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 echo `aws secretsmanager get-secret-value --region us-east-1 --secret-id docker_login_password| jq --raw-output '.SecretString'| jq --raw-output '."docker_login_password"'`| docker login --username danieletouk3 --password-stdin 
 
