f5f0b43985900c42fa05a21325454d966877f3f5
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-02-28 12:41:14 -0500

    updates

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -3,7 +3,7 @@
 echo `aws secretsmanager get-secret-value --region us-east-1 --secret-id docker_login_password| jq --raw-output '.SecretString'| jq --raw-output '."docker_login_password"'`| docker login --username danieletouk3 --password-stdin 
 
 repo_name=danieletouk3
-image_name=app
+image_name=app2
 num=`docker images | awk '{print $2}' | head -2 | tail -1`
 version=$(echo $num + 1 | bc -l)
 
